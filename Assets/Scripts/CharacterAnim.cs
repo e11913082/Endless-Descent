6dfae1a256ea9199f0ab513f903836af6dfed6b1
Severin Hager-Roiser <severinhr1@gmail.com>
2024-10-25 00:04:26 +0200

    Included enemy asset; copied scripts from 2d template to Scripts folder (these we can edit) Created Severins Playground scene

---
+++ Assets/Scripts/CharacterAnim.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using UnityEngine;
+
+namespace EndlessDescent
+{
+    [RequireComponent(typeof(PlayerCharacter))]
+    [RequireComponent(typeof(Animator))]
+    public class CharacterAnim : MonoBehaviour
+    {
+        private PlayerCharacter character;
+        private CharacterHoldItem character_item;
+        private Animator animator;
+        private SpriteRenderer spriteRenderer;
+        public bool flipSpriteOnTurn = false;
+        private int side;
+
+        void Awake()
+        {
+            character = GetComponent<PlayerCharacter>();
+            character_item = GetComponent<CharacterHoldItem>();
+            animator = GetComponent<Animator>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        void Update()
+        {
+            //Anims
+            animator.SetFloat("Speed", character.GetMove().magnitude);
+            side = character.GetSideAnim();
+            animator.SetInteger("Side", side);
+            if (flipSpriteOnTurn == true)
+                spriteRenderer.flipX = (side == 3)? true : false;
+            if(character_item != null)
+                animator.SetBool("Hold", character_item.GetHeldItem() != null);
+        }
+        
+    }
+
+}
