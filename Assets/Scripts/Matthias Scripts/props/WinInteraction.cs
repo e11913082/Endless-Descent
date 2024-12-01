274577d99bfa688e4107ca495c78da0dd8ee6439
Qui-B <guglermatthias@gmail.com>
2024-12-01 22:39:11 +0100

    added missing script

---
+++ Assets/Scripts/Matthias Scripts/props/WinInteraction.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WinInteraction : MonoBehaviour
+{
+    private GameObject winBanner;
+    void Start()
+    {
+        winBanner = transform.Find("WinBanner").gameObject;
+        winBanner.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //TODO play secret sound
+        if (other.CompareTag("Player"))
+        {
+            Time.timeScale = 0f;
+            winBanner.SetActive(true);
+        }
+    }
+}
