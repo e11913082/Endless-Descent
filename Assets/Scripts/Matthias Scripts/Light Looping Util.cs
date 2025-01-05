5d3730e462f7ff8fe45e38d22f771fa4031597ef
Qui-B <guglermatthias@gmail.com>
2025-01-05 18:40:45 +0100

    added lantern mechanic

---
+++ Assets/Scripts/Matthias Scripts/Light Looping Util.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class LightLoopingUtil : MonoBehaviour
+{
+    private int lv = 0;
+
+    private Light2D outerLight;
+
+    private List<GameObject> lanterns;
+
+    private CircleCollider2D lightCollider;
+
+    //multiplier for transitioning to lv1
+    const float LANTERN_INNER_MULTIPLIER = 0.85f;
+    const float LANTERN_OUTER_MULTIPLIER = 0.7f;
+    const float OUTER_MULTIPLIER = 0.6f;
+    const float LIGHT_INTENSITY = 0.45f;
+
+    void Awake()
+    {
+        Transform outerLightTrans = transform.Find("outer light");
+        if (outerLightTrans != null)
+        {
+            outerLight = outerLightTrans.GetComponent<Light2D>();
+            lightCollider = outerLightTrans.GetComponent<CircleCollider2D>();
+        }
+        else
+        {
+            lightCollider = gameObject.GetComponent<CircleCollider2D>();
+        }
+    }
+
+    //TODO CLEANUP
+    private void Start() //only for testing remove afterwards
+    {
+        //SetLanternLv(2);
+    }
+
+
+    public List<GameObject> GetLanterns() //searches for all small lamps in the gameobject its direct childs
+    {
+        Debug.Log("Get LAnterns executed");
+        List<GameObject> outputLanterns = new List<GameObject>();
+        if (gameObject.name.Contains("small"))
+        {
+            outputLanterns.Add(gameObject);
+        }
+        foreach (Transform child in transform)
+        {
+            if (child.name.Contains("small"))
+            {
+                outputLanterns.Add(child.gameObject);
+            }
+        }
+        return outputLanterns;
+    }
+
+    public void SetLanternLv(int lv)
+    {
+        lanterns = GetLanterns();
+        if (lv == 1)
+        {
+            foreach (GameObject lantern in lanterns)
+            {
+                lantern.GetComponent<Animator>().SetInteger("lv", 1);
+                Light2D lanternOuterLight = lantern.GetComponent<Light2D>();
+                Light2D lanternInnerLight = lantern.transform.GetChild(0).GetComponent<Light2D>();
+
+                lanternOuterLight.pointLightOuterRadius *= LANTERN_OUTER_MULTIPLIER;
+                lanternOuterLight.pointLightInnerRadius *= LANTERN_OUTER_MULTIPLIER;
+                lanternOuterLight.intensity *= LIGHT_INTENSITY;
+
+                lanternInnerLight.pointLightOuterRadius *= LANTERN_INNER_MULTIPLIER;
+                lanternInnerLight.pointLightInnerRadius *= LANTERN_INNER_MULTIPLIER;
+                lanternInnerLight.intensity *= LIGHT_INTENSITY;
+            }
+
+            if (outerLight != null)
+            {
+                lightCollider.radius *= OUTER_MULTIPLIER;
+                outerLight.pointLightOuterRadius *= OUTER_MULTIPLIER;
+                outerLight.pointLightInnerRadius *= OUTER_MULTIPLIER;
+                outerLight.intensity *= LIGHT_INTENSITY;
+            }
+            else
+            {
+                lightCollider.radius *= LANTERN_OUTER_MULTIPLIER;
+            }
+        }
+        else if (lv == 2)
+        {
+            foreach(GameObject lantern in lanterns)
+            {
+                lantern.GetComponent<Animator>().SetInteger("lv", 2);
+                lantern.GetComponent<Light2D>().intensity = 0;
+                lantern.transform.GetChild(0).GetComponent<Light2D>().intensity = 0;
+            }
+            
+            Destroy(lightCollider);
+            if (outerLight != null)
+            {
+                outerLight.intensity = 0;
+            }
+        }
+    }
+}
--- Assets/Scripts/Matthias Scripts/props/looping interaction.cs
+++ Assets/Scripts/Matthias Scripts/props/looping interaction.cs
@@ -15,43 +15,43 @@ public class LoopInteraction : MonoBehaviour
     private static Vector3 spawnPos;
     private static int loopCount = 0;
 
-    public GameObject loopEnemies;
+    public GameObject loopEnemies; //parentobj of all additional loop enemies
+    public GameObject lanterns; //parentobj of all lantern groups
+
     private LayerMask playerLayer;
  
-    private void Awake()
+    private void Start()
     {
-        if (instance == null) //case: game start first iteration
+        playerLayer = LayerMask.GetMask("Player");
+        if (instance == null) //case: game start (first iteration)
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
-            spawnPos = GameObject.Find("Main Character/Managers").transform.position;
 
+            curPlayer = GameObject.Find("Main Character");
+            spawnPos = GameObject.Find("Main Character").transform.position;
         }
         else //case: player is looping
         {
             loopCount++;
+            
             curPlayer.transform.position = spawnPos;
+            //Debug.Log("Transfered player to pos: " + spawnPos.x + "/" + spawnPos.y + "/" + spawnPos.z);
+
+            HandleEnemies();
+            HandleLanterns();
+
             Destroy(gameObject);
         }
-        HandleEnemies();
-    }
-    void Start()
-    {
-        playerLayer = LayerMask.GetMask("Player");
+        
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other) //reload scene when player enters
     {
         if ((playerLayer.value & (1 << other.gameObject.layer)) != 0)
         {
-            curPlayer = GameObject.Find("Main Character/Managers");
-            ReloadScene();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
-    }
-
-    void ReloadScene()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     void HandleEnemies() //fills the map with an appropriate amount of enemies based on the number if the loop
@@ -59,7 +59,6 @@ public class LoopInteraction : MonoBehaviour
         //DeactivateAllLoopEnemies();
         float spawnThreshold = (10f - 2*loopCount) / 10f;
         int enemyCount = loopEnemies.transform.childCount;
-        Debug.Log("New spawnThreshold: " + spawnThreshold);
 
         for (int i = 0; i < enemyCount; i++)
         {
@@ -71,14 +70,51 @@ public class LoopInteraction : MonoBehaviour
             }
         }
     }
-    void DeactivateAllLoopEnemies()
-    {
-        int childCount = loopEnemies.transform.childCount;
 
-        for (int i = 0; i < childCount; i++)
+    void HandleLanterns()
+    {
+        int lanternGroupCount = lanterns.transform.childCount;
+        for (int i = 0; i < lanternGroupCount; i++)
         {
-            GameObject enemy = loopEnemies.transform.GetChild(i).gameObject;
-            enemy.SetActive(false);
+            HandleLanternGroup(lanterns.transform.GetChild(i).gameObject);
         }
     }
+
+    void HandleLanternGroup(GameObject lanternGroup)
+    {
+        int lanternCount = lanternGroup.transform.childCount;
+        int activeLanterns = lanternCount; //to ensure one lantern is at least active in every group
+
+        float lv1Threshold = (10f - 2*loopCount) / 10f;
+        float lv2Threshold = (10f - loopCount) / 10f;
+        for (int i = 0; i < lanternCount; i++)
+        {
+            float randomeFloat = UnityEngine.Random.Range(0.0f, 1.0f);
+            LightLoopingUtil lanternUtil;
+
+            if(randomeFloat > lv2Threshold && activeLanterns > 1)
+            {
+                Debug.Log("setting lantern to 2");
+                lanternUtil = lanternGroup.transform.GetChild(i).gameObject.GetComponent<LightLoopingUtil>();
+                lanternUtil.SetLanternLv(2);
+                activeLanterns--;
+            }
+            else if(randomeFloat > lv1Threshold)
+            {
+                Debug.Log("setting lantern to 1");
+                lanternUtil = lanternGroup.transform.GetChild(i).gameObject.GetComponent<LightLoopingUtil>();
+                lanternUtil.SetLanternLv(1);
+            }
+        }
+    }
+    //void DeactivateAllLoopEnemies()
+    //{
+    //    int childCount = loopEnemies.transform.childCount;
+
+    //    for (int i = 0; i < childCount; i++)
+    //    {
+    //        GameObject enemy = loopEnemies.transform.GetChild(i).gameObject;
+    //        enemy.SetActive(false);
+    //    }
+    //}
 }
